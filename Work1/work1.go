5db37c6ae8a9d87dc077d19eae58da21a37f2eed
CQUPTWYZ <981774028@qq.com>
2021-12-21 23:26:50 +0800

    go

---
+++ Work1/work1.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+func main() {
+	var ch1=make(chan string)
+	var ch2 = make(chan string)
+	var ch3 = make(chan string)
+	var ch4 = make(chan string)
+	for i := 0; i < 10; i++ {
+		go func() {
+			ch1<-"张三"
+		}()
+		go func() {
+			ch2<-"李四"
+		}()
+		go func() {
+			ch3<-"王五"
+		}()
+		go func() {
+			ch4<-"赵六"
+		}()
+	}
+	for i:=0;i<10;i++{
+		fmt.Println(<-ch1)
+		fmt.Println(<-ch2)
+		fmt.Println(<-ch3)
+		fmt.Println(<-ch4)
+	}
+} 
