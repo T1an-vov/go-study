ad3cf872b2d3116e2f58e0a9beec511662d4cc04
CQUPTWYZ <981774028@qq.com>
2021-12-21 22:58:24 +0800

    1

--- Work5/go/module/Courses.go
+++
@@ -1,9 +0,0 @@
-package module
-
-type Course struct {
-	ID         uint
-	Coursename string `form:"coursename" json:"coursename" binding:"required" gorm:"unique"` //课程名
-	Credit     int    `form:"credit" json:"credit" bind:"required"` //学分
-	Number     int `form:"number" json:"number"`  //目前选课人数
-	Maxnumber  int `form:"maxnumber" json:"maxnumber" binding:"required"`  //最大选课人数
-}
